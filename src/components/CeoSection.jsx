2a5f719ab4a01b90182788693ba39c03d4cae741
Ivan <coderivan49@gmail.com>
2024-06-01 13:04:40 +0200

    add more changes

--- src/components/CeoSection.jsx
+++ src/components/CeoSection.jsx
@@ -1,36 +1,3 @@
-// function CeoSection() {
-// 	return (
-// 		<div className=" items-start flex mt-16 bg-[url('/public/ceoBgImg.svg')] mb-11 pt-[8rem] p-16 gap-x-16  max-md:flex-col  max-md:h-full md:max-xl:h-full md:max-xl:gap-0 md:max-xl:bg-cover md:max-lg:pt-0">
-// 			<div className='w-1/2 max-md:w-full'>
-// 				<h1 className='mb-8 font-bold leading-10 font-koho '>
-// 					<span className='text-3xl text-white max-md:text-xl'>
-// 						Experience Unlimited Learning
-// 					</span>
-// 					<br className='mt-2 max-md:hidden' />
-// 					<span className='text-3xl text-white max-md:text-xl'>
-// 						Potential
-// 					</span>
-// 					<span className='pl-1  text-[#24D198] text-3xl max-md:text-xl'>
-// 						with SPidEd!
-// 					</span>
-// 				</h1>
-// 				<p className='mb-8 font-sans text-xl font-light text-white max-md:text-center max-md:text-[15px]'>
-// 					We know that if you&apos;re reading this, it&apos;s because
-// 					you&apos;re eager to learn, and you&apos;re in the right place.
-// 					Our goal is to train individuals like you and help them gain
-// 					important and useful skills. Dive into a world of endless
-// 					learning opportunities with SPidEd.
-// 				</p>
-// 			</div>
-// 			<img
-// 				src='/ceoimge.png'
-// 				className='object-contain w-[46%] max-md:w-full'
-// 			/>
-// 		</div>
-// 	);
-// }
-
-// export default CeoSection;
 import { useState, useEffect } from 'react';
 
 function CeoSection() {
