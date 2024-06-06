3586c40191a64f75a698bc5941f892de138e1481
stevenmukama1@gmail.com <stevenmukama1@gmail.com>
2024-06-06 12:39:16 +0200

    add more changes

--- src/pages/CertificationPage.jsx
+++ src/pages/CertificationPage.jsx
@@ -21,7 +21,7 @@ function CertificationPage() {
 							placeholder='Enter verification Number|'
 							className='w-full placeholder:bg-white placeholder:text-[##A6A6A6] h-12 '
 						/>
-						<button className='bg-[#000] text-white px-4 py-2 rounded-md mt-4'>
+						<button className='bg-[#000] text-white px-4 py-4 rounded-md'>
 							Verify
 						</button>
 					</div>
