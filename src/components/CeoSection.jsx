dac01e149278cc4482c8f5f9ed79455ae7ed5bbe
stevenmukama1@gmail.com <stevenmukama1@gmail.com>
2024-06-13 19:21:35 +0200

    add more changes

--- src/components/CeoSection.jsx
+++ src/components/CeoSection.jsx
@@ -15,7 +15,7 @@ function CeoSection() {
 		};
 	}, []);
 	return (
-		<div className="items-start flex mt-16 bg-cover mb-11 pt-[8rem] p-16 max-md:py-7 max-md:px-7 gap-x-16 max-md:flex-col max-md:pt-3 max-md: max-md:h-full md:max-xl:h-full md:max-xl:gap-0 bg-[url('/ceoBgImg.svg')] max-md:bg-[url('/phoneBg.svg')]">
+		<div className="items-start flex mt-16 bg-cover mb-11 pt-[8rem] p-16 max-md:py-7 max-md:px-7 gap-16 max-md:flex-col max-md:pt-3 max-md: max-md:h-full md:max-xl:h-full md:max-xl:gap-3 bg-[url('/ceoBgImg.svg')] max-md:bg-[url('/phoneBg.svg')]">
 			<div className='w-1/2 max-md:w-full max-md:pt-20'>
 				{windowWidth <= 768 ? (
 					<>
@@ -58,7 +58,7 @@ function CeoSection() {
 			</div>
 			<img
 				src='/ceoimge.png'
-				className='object-contain w-[46%] max-md:w-full'
+				className='object-contain w-[50%] max-md:w-full'
 			/>
 		</div>
 	);
